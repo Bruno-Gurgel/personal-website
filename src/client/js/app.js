8564ac229b3aeca2dd27e61f6d9cbf8b0eaa32ac
Bruno Gurgel <bmg1612@gmail.com>
2020-12-28 20:25:09 -0300

    chore: Removed projects from navigation ->
    - The proejcts can be seen on the main page, the nav item
    was unnecessary;
    - I changed the heading of the project outside of the figcaption,
    so that it can appear initially.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,27 +1,11 @@
 const appFunctions = document.addEventListener('DOMContentLoaded', () => {
-  /* const model = {
-    
-  }; */
+  const model = {};
 
   const controller = {
     init() {
       this.repoButtons = document.getElementsByClassName('projects__repo');
-      this.projects = document.getElementsByClassName('projects__figure');
-      this.projectsList = document.querySelector('.nav__submenu');
 
-      view.init();
-
-      this.createSubMenu();
       this.openRepo();
-    },
-    createSubMenu() {
-      const fragment = document.createDocumentFragment();
-      for (const project of this.projects) {
-        const newElement = document.createElement('li');
-        newElement.innerHTML = `<a class="nav__submenu__item"  href="#"> ${project.dataset.submenu}</a>`;
-        fragment.appendChild(newElement);
-        this.projectsList.appendChild(fragment);
-      }
     },
     openRepo() {
       for (const btn of this.repoButtons) {
@@ -34,17 +18,8 @@ const appFunctions = document.addEventListener('DOMContentLoaded', () => {
   };
 
   const view = {
-    init() {
-      this.navProjects = document.querySelector('.--project');
-      this.projectsList = document.querySelector('.nav__submenu');
-
-      this.render();
-    },
-    render() {
-      this.navProjects.addEventListener('click', () => {
-        this.projectsList.classList.toggle('visible');
-      });
-    },
+    init() {},
+    render() {},
   };
 
   controller.init();
