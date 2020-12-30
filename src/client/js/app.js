d8766bc3e9189f2af01f5669d6836ac7e95b7f84
Bruno Gurgel <bmg1612@gmail.com>
2020-12-30 15:21:36 -0300

    chore: Removed openRepo() since now it is an anchor tag

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,19 +2,7 @@ const appFunctions = document.addEventListener('DOMContentLoaded', () => {
   const model = {};
 
   const controller = {
-    init() {
-      this.repoButtons = document.getElementsByClassName('projects__repo');
-
-      this.openRepo();
-    },
-    openRepo() {
-      for (const btn of this.repoButtons) {
-        btn.addEventListener('click', () => {
-          console.log(btn);
-          window.open(`https://github.com/Bruno-Gurgel/${btn.name}`);
-        });
-      }
-    },
+    init() {},
   };
 
   const view = {
