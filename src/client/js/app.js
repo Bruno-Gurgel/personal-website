1e08402c6a19c58090b32262c5100896bb0402bc
Bruno Gurgel <bmg1612@gmail.com>
2021-01-04 14:18:38 -0300

    feat: Added function to make hover state accessible with tab presses
    - By default, hover states are inaccessible for keyboard only users,
    I added a function to fix this.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,12 +2,48 @@ const appFunctions = document.addEventListener('DOMContentLoaded', () => {
   const model = {};
 
   const controller = {
-    init() {},
+    init() {
+      this.projectThumbnails = document.getElementsByClassName(
+        'projects__figure'
+      );
+      this.lastButtonsOfThumbnails = document.getElementsByClassName(
+        'projects__button'
+      );
+
+      view.init();
+    },
+    /*
+    Hover states are inaccessible for keyboard users initially.
+    This function solves this problem
+    */
+    tabWithinHover() {
+      for (const project of this.projectThumbnails) {
+        project.addEventListener('blur', () => {
+          /* 
+          When the user leaves the focus of the figure element, the display of the respective figcaption is set to block with an inline style, otherwise the figcaptio would not appear and by pressing tab the user would go to the next figure
+          */
+          const figCaption = project.lastElementChild;
+          figCaption.style.display = 'block';
+          for (const lastButton of this.lastButtonsOfThumbnails) {
+            /*  
+            When the user leaves the focus of the last button inside the figcaption, remove the inline style of the figcaption
+            */
+            lastButton.onblur = () => {
+              figCaption.style.display = '';
+            };
+          }
+        });
+      }
+    },
   };
 
   const view = {
-    init() {},
-    render() {},
+    init() {
+      this.render();
+    },
+    render() {
+      controller.tabWithinHover();
+    },
   };
 
   controller.init();
