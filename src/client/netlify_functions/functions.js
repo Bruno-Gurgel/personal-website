d525b3c4c0594848d130098eedd94a717d8ecb0e
Bruno Gurgel <bmg1612@gmail.com>
2021-01-12 20:10:09 -0300

    test: Testing lambda functions

--- src/client/netlify_functions/functions.js
+++ src/client/netlify_functions/functions.js
@@ -7,6 +7,8 @@ const serverless = require('serverless-http');
 
 const app = express();
 
+const router = express.Router();
+
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -21,28 +23,14 @@ app.use(express.static('dist'));
 
 console.log(__dirname);
 
-app.get('/', (req, res) =>
-  res.sendFile(path.resolve('src/client/views/index.html'))
-);
-
 // Empty JS object to act as endpoint for all routes
 let allData = {};
 
-// Spin up the server
-/* const port = 8000;
-app.listen(process.env.PORT || port, () => {
-  console.log(`App is listening on port ${port}`);
-}); */
+router.get('/', (req, res) =>
+  res.sendFile(path.resolve('src/client/views/index.html'))
+);
 
-module.exports.handler = serverless(app);
-
-const openWeatherAPIKey = process.env.OPENWEATHER_API_KEY;
-const meaningCloudAPIKey = process.env.MEANINGCLOUD_API_KEY;
-const weatherBitAPIKey = process.env.WEATHERBIT_API_KEY;
-const pixabayAPIKey = process.env.PIXABAY_API_KEY;
-
-// Sending the API key to the client
-app.get('/api', (req, res) =>
+router.get('/api', (req, res) =>
   res.send({
     openWeatherKey: openWeatherAPIKey,
     meaningCloudKey: meaningCloudAPIKey,
@@ -51,7 +39,7 @@ app.get('/api', (req, res) =>
   })
 );
 
-app.post('/data', (req, res) => {
+router.post('/data', (req, res) => {
   allData = {
     weatherAppData: {
       date: req.body.date,
@@ -77,6 +65,62 @@ app.post('/data', (req, res) => {
   res.send(allData);
 });
 
-app.get('/UIdata', function (request, response) {
+router.get('/UIdata', function (request, response) {
   response.send(allData);
 });
+/* app.get('/', (req, res) =>
+  res.sendFile(path.resolve('src/client/views/index.html'))
+); */
+
+// Spin up the server
+/* const port = 8000;
+app.listen(process.env.PORT || port, () => {
+  console.log(`App is listening on port ${port}`);
+}); */
+
+module.exports.handler = serverless(app);
+
+const openWeatherAPIKey = process.env.OPENWEATHER_API_KEY;
+const meaningCloudAPIKey = process.env.MEANINGCLOUD_API_KEY;
+const weatherBitAPIKey = process.env.WEATHERBIT_API_KEY;
+const pixabayAPIKey = process.env.PIXABAY_API_KEY;
+
+// Sending the API key to the client
+/* app.get('/api', (req, res) =>
+  res.send({
+    openWeatherKey: openWeatherAPIKey,
+    meaningCloudKey: meaningCloudAPIKey,
+    weatherBitKey: weatherBitAPIKey,
+    photoKey: pixabayAPIKey,
+  })
+); */
+
+/* app.post('/data', (req, res) => {
+  allData = {
+    weatherAppData: {
+      date: req.body.date,
+      temperature: req.body.temperature,
+      user_response: req.body.user_response,
+    },
+    newsAnalyzerData: {
+      agreement: req.body.agreement,
+      subjectivity: req.body.subjectivity,
+      confidence: req.body.confidence,
+      irony: req.body.irony,
+    },
+    travelAppData: {
+      city_name: req.body.city_name,
+      country_code: req.body.country_code,
+      temperature: req.body.temp,
+      app_temp: req.body.app_temp,
+      description: req.body.description,
+      photo: req.body.photo,
+    },
+  };
+  console.log(allData);
+  res.send(allData);
+}); */
+
+/* app.get('/UIdata', function (request, response) {
+  response.send(allData);
+}); */
