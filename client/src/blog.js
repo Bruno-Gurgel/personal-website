9285efc578e53eb8da2374522f341c676b49e99d
Bruno Gurgel <bmg1612@gmail.com>
2021-01-24 11:20:57 -0300

    chore: Added fallback images for old browsers

--- client/src/blog.js
+++ client/src/blog.js
@@ -2,11 +2,21 @@ import './style/blog/main.css';
 
 import blogPostImage from './img/blog/blog-post.webp';
 import blogAvatarImage from './img/blog/avatar.webp';
+import fallbackBlogPostImage from './img/blog/fallback/blog-post.jpg';
+import fallbackBlogAvatarImage from './img/blog/fallback/avatar.png';
 
-const images = document.getElementsByClassName('blog-img');
-for (const image of images) {
-  image.src = blogPostImage;
+const images = document.getElementsByClassName('--webp');
+for (const source of images) {
+  source.srcset = blogPostImage;
 }
 
-const avatar = document.querySelector('.img');
-avatar.src = blogAvatarImage;
+const fallbackImages = document.getElementsByClassName('--fallback');
+for (const image of fallbackImages) {
+  image.src = fallbackBlogPostImage;
+}
+
+const avatar = document.querySelector('.avatar-img--webp');
+avatar.srcset = blogAvatarImage;
+
+const fallbackAvatar = document.querySelector('.avatar-img--fallback');
+fallbackAvatar.src = fallbackBlogAvatarImage;
